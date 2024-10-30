b2b26ab7a5b19d7e743f3306da4af3ec3760f472
Bercan aydin <143941941+maymun1234@users.noreply.github.com>
2024-10-30 14:26:45 +0300

    aaa

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+# app/views.py
 
-# Create your views here.
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def user_profile(request):
+    return render(request, 'profile.html')  
